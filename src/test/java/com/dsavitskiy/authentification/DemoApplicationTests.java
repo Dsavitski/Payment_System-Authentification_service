7a061d5af1a63d8023e47c8aa7a3bf2fdb4079f1
Denis Savitskiy <savickijdenis69@gmail.com>
2026-07-26 23:30:41 +0300

    Create authentification service

---
+++ src/test/java/com/dsavitskiy/authentification/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsavitskiy.authentification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
